22594611e3524c4300dc8c4a3f57b213bee3b148
OksanaFedyukova <avseniya222@gmail.com>
2023-03-28 10:37:40 +0200

    install

--- spa/src/store/index.js
+++ spa/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import actions from './';
+import actions from './actions';
 import mutations from './mutations';
 import getters from './getters';
 
